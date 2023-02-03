bcb62eb5fe9bf8e2da82fa14f82902ecbb689958
avram-beni11 <avrambeni11@gmail.com>
2023-02-03 18:22:38 +0000

    app.js

---
+++ app.js
@@ -0,0 +1,35 @@
+const app = Vue.createApp({
+    el: '#app',
+    data() {
+        return {
+            cart: [],
+            sortedPrice: true,
+            showProducts: 0,
+            sortingOrder: "asc",
+            sortBy: "name",
+            products: "http://lessonapp-env.eba-uiw2prds.us-east-1.elasticbeanstalk.com/collections/products",
+            orders:
+            {
+                name: '',
+                phone: '',
+                slots: [],
+            },
+            lessonsData: [],
+            cartData: []
+        }
+    },
+
+    created() {
+        //fetch("http://localhost:3000/collections/products").then(
+            fetch("http://lessonapp-env.eba-uiw2prds.us-east-1.elasticbeanstalk.com/collections/products").then(
+                function (response) {
+                    response.json().then(
+                        function (json) {
+                            webstore.products = json;
+                        }
+                    )
+                }
+            );
+    }
+
+});
