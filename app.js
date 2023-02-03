9adeb8d586766afab17f943a70cb7fbacd3838be
avram-beni11 <avrambeni11@gmail.com>
2023-02-03 21:36:06 +0000

    1

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const app = Vue.createApp({
             showProducts: 0,
             sortingOrder: "asc",
             sortBy: "name",
-            // products: "http://lessonapp-env.eba-uiw2prds.us-east-1.elasticbeanstalk.com/collections/products",
-            products: "http://localhost:3000/collections/products",
+            products: "http://lessonapp-env.eba-uiw2prds.us-east-1.elasticbeanstalk.com/collections/products",
+            //products: "http://localhost:3000/collections/products",
             orders:
             {
                 name: '',
@@ -21,52 +21,52 @@ const app = Vue.createApp({
     },
 
     created() {
-        fetch("http://localhost:3000/collections/products").then(
-            //fetch("http://lessonapp-env.eba-uiw2prds.us-east-1.elasticbeanstalk.com/collections/products").then(
-                function (response) {
-                    response.json().then(
-                        function (json) {
-                            Webstore.products = json;
-                        }
-                    )
-                }
-            );
-    //     fetch("http://lessonapp-env.eba-uiw2prds.us-east-1.elasticbeanstalk.com/collections/products")
-    //         .then(response => response.json())
-    //         .then(data => {
-    //             // Loop through the JSON data and insert it into the HTML
-    //             data.forEach(product => {
-    //                 let container = document.getElementById("productCards");
-    //                 let productHTML = `
-    //     <div class="product">
-    //       <h2>${product.name}</h2>
-    //       <p>${product.description}</p>
-    //       <p>Price: ${product.price}</p>
-    //       <p>Location: ${product.location}</p>
-    //       <p>Spaces: ${product.spaces}</p>
-    //       <img src="${product.image}" alt="${product.name}">
-    //     </div>
-    //   `;
-    //                 container.innerHTML += productHTML;
-    //             });
-    //         });
-    // }
-    // fetch("http://lessonapp-env.eba-uiw2prds.us-east-1.elasticbeanstalk.com/collections/products")
-    // .then(response => response.json())
-    // .then(data => {
-    //   let products = data.products;
-    //   let result = "";
-    //   products.forEach(function(product) {
-    //     result += `
-    //       <h2>${product.name}</h2>
-    //       <p>${product.description}</p>
-    //       <p>Price: ${product.price}</p>
-    //       <p>Location: ${product.location}</p>
-    //       <p>Spaces: ${product.spaces}</p>
-    //     `;
-    //   });
-    //   document.getElementById("productCards").innerHTML = result;
-    // });
-}
+        //fetch("http://localhost:3000/collections/products").then(
+        fetch("http://lessonapp-env.eba-uiw2prds.us-east-1.elasticbeanstalk.com/collections/products").then(
+            function (response) {
+                response.json().then(
+                    function (json) {
+                        Webstore.products = json;
+                    }
+                )
+            }
+        );
+        //     fetch("http://lessonapp-env.eba-uiw2prds.us-east-1.elasticbeanstalk.com/collections/products")
+        //         .then(response => response.json())
+        //         .then(data => {
+        //             // Loop through the JSON data and insert it into the HTML
+        //             data.forEach(product => {
+        //                 let container = document.getElementById("productCards");
+        //                 let productHTML = `
+        //     <div class="product">
+        //       <h2>${product.name}</h2>
+        //       <p>${product.description}</p>
+        //       <p>Price: ${product.price}</p>
+        //       <p>Location: ${product.location}</p>
+        //       <p>Spaces: ${product.spaces}</p>
+        //       <img src="${product.image}" alt="${product.name}">
+        //     </div>
+        //   `;
+        //                 container.innerHTML += productHTML;
+        //             });
+        //         });
+        // }
+        // fetch("http://lessonapp-env.eba-uiw2prds.us-east-1.elasticbeanstalk.com/collections/products")
+        // .then(response => response.json())
+        // .then(data => {
+        //   let products = data.products;
+        //   let result = "";
+        //   products.forEach(function(product) {
+        //     result += `
+        //       <h2>${product.name}</h2>
+        //       <p>${product.description}</p>
+        //       <p>Price: ${product.price}</p>
+        //       <p>Location: ${product.location}</p>
+        //       <p>Spaces: ${product.spaces}</p>
+        //     `;
+        //   });
+        //   document.getElementById("productCards").innerHTML = result;
+        // });
+    }
 
 });
