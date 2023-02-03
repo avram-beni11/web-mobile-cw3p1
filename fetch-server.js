e028fe26f4300e2e5ace3d4148eee82e65f96dfa
avram-beni11 <avrambeni11@gmail.com>
2023-02-03 15:19:36 +0000

    finished server + postman

--- fetch-server.js
+++ fetch-server.js
@@ -24,30 +24,92 @@ app.use(express.json());
 
 app.set('json spaces', 3);
 
-// app.param('collectionName'
-//    , function (req, res, next, collectionName) {
-//       req.collection = db.collection(collectionName);
-//       return next();
-//    });
+app.param('collectionName'
+   , function (req, res, next, collectionName) {
+      req.collection = db.collection(collectionName);
+      return next();
+   });
 
-// app.get('/collections/:collectionName'
-//    , function (req, res, next) {
-//       req.collection.find({}).toArray(function (err, results) {
-//          if (err) {
-//             return next(err);
-//          }
-//          res.send(results);
-//       });
-//    });
+app.get('/', (req, res) => {
+   res.send('Hello World!');
+});
 
 app.get('/collections/:collectionName'
    , function (req, res, next) {
-      req.collection.find({}, { limit: 3, sort: [["price", -1]] }).toArray(function (err, results) {
+      req.collection.find({}).toArray(function (err, results) {
          if (err) {
             return next(err);
          }
          res.send(results);
       });
+   });
+
+app.get('/collections/:collectionName/:max/:sortAspect/:sortAscDesc'
+   , function (req, res, next) {
+      // TODO: Validate params
+      var max = parseInt(req.params.max, 10); // base 10
+      let sortDirection = 1;
+      if (req.params.sortAscDesc === "desc") {
+         sortDirection = -1;
+      }
+      req.collection.find({}, {
+         limit: max, sort: [[req.params.sortAspect,
+            sortDirection]]
+      }).toArray(function (err, results) {
+         if (err) {
+            return next(err);
+         }
+         res.send(results);
+      });
+   });
+
+app.get('/collections/:collectionName/:id'
+   , function (req, res, next) {
+      req.collection.findOne({ _id: new ObjectId(req.params.id) }, function (err, results) {
+         if (err) {
+            return next(err);
+         }
+         res.send(results);
+      });
+   });
+
+app.delete('/collections/:collectionName/:id'
+   , function (req, res, next) {
+      req.collection.deleteOne(
+         { _id: new ObjectId(req.params.id) }, function (err, result) {
+            if (err) {
+               return next(err);
+            } else {
+               res.send((result.deletedCount === 1) ? { msg: "success" } : { msg: "error" });
+            }
+         }
+      );
+   });
+
+app.post('/collections/:collectionName'
+   , function (req, res, next) {
+      // TODO: Validate req.body
+      req.collection.insertOne(req.body, function (err, results) {
+         if (err) {
+            return next(err);
+         }
+         res.send(results);
+      });
+   });
+
+app.put('/collections/:collectionName/:id'
+   , function (req, res, next) {
+      // TODO: Validate req.body
+      req.collection.updateOne({ _id: new ObjectId(req.params.id) },
+         { $set: req.body },
+         { safe: true, multi: false }, function (err, result) {
+            if (err) {
+               return next(err);
+            } else {
+               res.send((result.matchedCount === 1) ? { msg: "success" } : { msg: "error" });
+            }
+         }
+      );
    });
 
 const port = process.env.PORT || 3000;
