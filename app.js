8f5aae705e370fef360d5c09073a3acc98f8efdf
avram-beni11 <avrambeni11@gmail.com>
2023-02-03 21:35:14 +0000

    changes

--- app.js
+++ app.js
@@ -26,11 +26,47 @@ const app = Vue.createApp({
                 function (response) {
                     response.json().then(
                         function (json) {
-                            webstore.products = json;
+                            Webstore.products = json;
                         }
                     )
                 }
             );
-    }
+    //     fetch("http://lessonapp-env.eba-uiw2prds.us-east-1.elasticbeanstalk.com/collections/products")
+    //         .then(response => response.json())
+    //         .then(data => {
+    //             // Loop through the JSON data and insert it into the HTML
+    //             data.forEach(product => {
+    //                 let container = document.getElementById("productCards");
+    //                 let productHTML = `
+    //     <div class="product">
+    //       <h2>${product.name}</h2>
+    //       <p>${product.description}</p>
+    //       <p>Price: ${product.price}</p>
+    //       <p>Location: ${product.location}</p>
+    //       <p>Spaces: ${product.spaces}</p>
+    //       <img src="${product.image}" alt="${product.name}">
+    //     </div>
+    //   `;
+    //                 container.innerHTML += productHTML;
+    //             });
+    //         });
+    // }
+    // fetch("http://lessonapp-env.eba-uiw2prds.us-east-1.elasticbeanstalk.com/collections/products")
+    // .then(response => response.json())
+    // .then(data => {
+    //   let products = data.products;
+    //   let result = "";
+    //   products.forEach(function(product) {
+    //     result += `
+    //       <h2>${product.name}</h2>
+    //       <p>${product.description}</p>
+    //       <p>Price: ${product.price}</p>
+    //       <p>Location: ${product.location}</p>
+    //       <p>Spaces: ${product.spaces}</p>
+    //     `;
+    //   });
+    //   document.getElementById("productCards").innerHTML = result;
+    // });
+}
 
 });
