0b84572b443876fe33672e3c4c178341b64cbecf
avram-beni11 <avrambeni11@gmail.com>
2023-02-04 18:12:52 +0000

    1

--- app.js
+++ app.js
@@ -1,112 +1,77 @@
-// const app = Vue.createApp({
-//     el: '#app',
-//     data() {
-//         return {
-//             cart: [],
-//             sortedPrice: true,
-//             showProducts: 0,
-//             sortingOrder: "asc",
-//             sortBy: "name",
-//             products: "http://lessonapp-env.eba-uiw2prds.us-east-1.elasticbeanstalk.com/collections/products",
-//             //products: "http://localhost:3000/collections/products",
-//             orders:
-//             {
-//                 name: '',
-//                 phone: '',
-//                 slots: [],
-//             },
-//             lessonsData: [],
-//             cartData: []
+const app = Vue.createApp({
+    el: '#app',
+    data() {
+        return {
+            cart: [],
+            sortedPrice: true,
+            showProducts: 0,
+            sortingOrder: "asc",
+            sortBy: "name",
+            products: "http://lessonapp-env.eba-uiw2prds.us-east-1.elasticbeanstalk.com/collections/products",
+            //products: "http://localhost:3000/collections/products",
+            orders:
+            {
+                name: '',
+                phone: '',
+                slots: [],
+            },
+            lessons: [],
+            cartData: []
+        }
+    },
+
+    created: function () {
+        fetch("http://lessonapp-env.eba-uiw2prds.us-east-1.elasticbeanstalk.com/collections/products")
+        .then((response) => response.json())
+        .then((lessons) => {
+          this.lessons = lessons;
+        });
+  
+      // this.getLessons();
+    },
+});
+
+// let Webstore = new Vue ({
+//     el: "#app",
+//     data: {
+//         sitename: "Lesson Store",
+//         showProduct: true,
+//         products: [],
+//         cart: [],
+//         order: {
+//             name: "",
+//             surname: "",
+//             address: "",
+//             city: "",
+//             state: "",
+//             zip: "",
+//             gift:"Do not send as gift",
+//             method: "Home",
+//             sendGift: "Send as gift",
+//             dontSendGift: "Do not send as gift"
+//         },
+//         states: {
+//             AL: "Alabama",
+//             AR: "Arizona",
+//             CA: "California",
+//             NV: "Nevada"
 //         }
 //     },
-
-//     created() {
-//         //fetch("http://localhost:3000/collections/products").then(
+//     created: function(){
 //         fetch("http://lessonapp-env.eba-uiw2prds.us-east-1.elasticbeanstalk.com/collections/products").then(
-//             function (response) {
-//                 response.json().then(
-//                     function (json) {
+//             function(response) {
+//                 response.json.then(
+//                     function(json){
+//                         alert(json);
 //                         Webstore.products = json;
 //                     }
 //                 )
 //             }
 //         );
-//         //     fetch("http://lessonapp-env.eba-uiw2prds.us-east-1.elasticbeanstalk.com/collections/products")
-//         //         .then(response => response.json())
-//         //         .then(data => {
-//         //             // Loop through the JSON data and insert it into the HTML
-//         //             data.forEach(product => {
-//         //                 let container = document.getElementById("productCards");
-//         //                 let productHTML = `
-//         //     <div class="product">
-//         //       <h2>${product.name}</h2>
-//         //       <p>${product.description}</p>
-//         //       <p>Price: ${product.price}</p>
-//         //       <p>Location: ${product.location}</p>
-//         //       <p>Spaces: ${product.spaces}</p>
-//         //       <img src="${product.image}" alt="${product.name}">
-//         //     </div>
-//         //   `;
-//         //                 container.innerHTML += productHTML;
-//         //             });
-//         //         });
-//         // }
-//         // fetch("http://lessonapp-env.eba-uiw2prds.us-east-1.elasticbeanstalk.com/collections/products")
-//         // .then(response => response.json())
-//         // .then(data => {
-//         //   let products = data.products;
-//         //   let result = "";
-//         //   products.forEach(function(product) {
-//         //     result += `
-//         //       <h2>${product.name}</h2>
-//         //       <p>${product.description}</p>
-//         //       <p>Price: ${product.price}</p>
-//         //       <p>Location: ${product.location}</p>
-//         //       <p>Spaces: ${product.spaces}</p>
-//         //     `;
-//         //   });
-//         //   document.getElementById("productCards").innerHTML = result;
-//         // });
-//     }
+//     },
+// })
 
-// });
-
-let Webstore = new Vue ({
-    el: "#app",
-    data: {
-        sitename: "Lesson Store",
-        showProduct: true,
-        products: [],
-        cart: [],
-        order: {
-            name: "",
-            surname: "",
-            address: "",
-            city: "",
-            state: "",
-            zip: "",
-            gift:"Do not send as gift",
-            method: "Home",
-            sendGift: "Send as gift",
-            dontSendGift: "Do not send as gift"
-        },
-        states: {
-            AL: "Alabama",
-            AR: "Arizona",
-            CA: "California",
-            NV: "Nevada"
-        }
-    },
-    created: function(){
-        fetch("http://lessonapp-env.eba-uiw2prds.us-east-1.elasticbeanstalk.com/collections/products").then(
-            function(response) {
-                response.json.then(
-                    function(json){
-                        alert(json);
-                        Webstore.products = json;
-                    }
-                )
-            }
-        );
-    },
-})
+console.log(fetch('http://lessonapp-env.eba-uiw2prds.us-east-1.elasticbeanstalk.com/collections/products')
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error(error)));
