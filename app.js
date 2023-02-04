908648edf642ed7cef6d0d06f5d952878976de1f
avram-beni11 <avrambeni11@gmail.com>
2023-02-04 19:17:05 +0000

    updated

--- app.js
+++ app.js
@@ -1,77 +1,43 @@
-const app = Vue.createApp({
+const app = new Vue({
     el: '#app',
     data() {
         return {
             cart: [],
             sortedPrice: true,
-            showProducts: 0,
+            showCart: true,
             sortingOrder: "asc",
             sortBy: "name",
-            products: "http://lessonapp-env.eba-uiw2prds.us-east-1.elasticbeanstalk.com/collections/products",
-            //products: "http://localhost:3000/collections/products",
+            url: "http://lessonapp-env.eba-uiw2prds.us-east-1.elasticbeanstalk.com/collections",
+            //url: "http://localhost:3000/collections",
             orders:
             {
                 name: '',
                 phone: '',
                 slots: [],
             },
-            lessons: [],
+            products: [],
             cartData: []
         }
     },
 
+    //fetching the products in json from the get path
     created: function () {
-        fetch("http://lessonapp-env.eba-uiw2prds.us-east-1.elasticbeanstalk.com/collections/products")
+      fetch("http://lessonapp-env.eba-uiw2prds.us-east-1.elasticbeanstalk.com/collections/products")
         .then((response) => response.json())
-        .then((lessons) => {
-          this.lessons = lessons;
+        .then((products) => {
+          this.products = products;
         });
-  
-      // this.getLessons();
+      this.getproducts();
     },
+    methods: {
+        getproducts() {
+            const url = `${this.url}/products/?search=${this.searchText}`;
+            fetch(url)
+              .then((response) => response.json())
+              .then((products) => {
+                this.products = products;
+              });
+        }
+    }        
+        
 });
-
-// let Webstore = new Vue ({
-//     el: "#app",
-//     data: {
-//         sitename: "Lesson Store",
-//         showProduct: true,
-//         products: [],
-//         cart: [],
-//         order: {
-//             name: "",
-//             surname: "",
-//             address: "",
-//             city: "",
-//             state: "",
-//             zip: "",
-//             gift:"Do not send as gift",
-//             method: "Home",
-//             sendGift: "Send as gift",
-//             dontSendGift: "Do not send as gift"
-//         },
-//         states: {
-//             AL: "Alabama",
-//             AR: "Arizona",
-//             CA: "California",
-//             NV: "Nevada"
-//         }
-//     },
-//     created: function(){
-//         fetch("http://lessonapp-env.eba-uiw2prds.us-east-1.elasticbeanstalk.com/collections/products").then(
-//             function(response) {
-//                 response.json.then(
-//                     function(json){
-//                         alert(json);
-//                         Webstore.products = json;
-//                     }
-//                 )
-//             }
-//         );
-//     },
-// })
-
-console.log(fetch('http://lessonapp-env.eba-uiw2prds.us-east-1.elasticbeanstalk.com/collections/products')
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.error(error)));
